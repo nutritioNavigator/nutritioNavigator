186e1d65e25d0bf8358f973a371ee7bc1049d570
Natalie Tso <natalietso16@gmail.com>
2021-04-15 14:56:53 -0400

    added footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ function App() {
   return (
     <div className="App">
       <h1>Nutrition Navigator</h1>
+      <footer>
+        <p>Created by <a href="#">Luis</a>, <a href="#">Natalie</a>, <a href="#">Sam</a>, and <a href="#">Yemisi</a> at <a href="https://junocollege.com/">Juno College</a></p>
+        <p>Powered by <a href="http://www.nutritionix.com/api">Nutritionix API</a></p>
+      </footer>
     </div>
   );
 }
